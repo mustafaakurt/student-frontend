2f9950e068dd2572dd0d27cdec5ca0a5febf0b6c
mustafaakurt <mustafaakurt@yandex.com>
2024-05-15 02:22:16 +0300

    action icons, add-student, view-all-students added

--- student-frontend/src/App.js
+++ student-frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import Navbar from "./component/common/Navbar";
 import StudentsView from './component/student/StudentsView';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddStudent from "./component/student/AddStudent";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route exact path="/view-students" element={<StudentsView />}>
           </Route>
+          <Route exact path="/add-student" element={<AddStudent />}>
+          </Route>
+
 
         </Routes>
       </Router>
--- student-frontend/src/component/student/StudentsView.js
+++ student-frontend/src/component/student/StudentsView.js
@@ -1,69 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { FaTrashAlt, FaEdit, FaEye } from "react-icons/fa";
 
 const StudentsView = () => {
+  const [students, setStudents] = useState([]);
 
-    const [students, setStudents] = useState([]);
+  useEffect(() => {
+    loadStudents();
+  }, []);
 
-    useEffect(() =>{
-        loadStudents();
-    }, [])
-    
-    const loadStudents = async() =>{
-        const result = await axios.get("http://localhost:8080/api/student",
-        {
-            validateStatus: () =>{
-                return true;
-            },
-        }
-        );
-        if (result.status ===  302){
-            setStudents(result.data)
-        }
-        
+  const loadStudents = async () => {
+    const result = await axios.get("http://localhost:8080/api/student", {
+      validateStatus: () => {
+        return true;
+      },
+    });
+    if (result.status === 302) {
+      setStudents(result.data);
     }
-    return (
-        <section>
-            <table className='table table-bordered table-hover'>
-                <thead>
-                    <tr className='text-center'>
-                        <th>ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Department</th>
-                        <th colSpan="3">Actions</th>
-                    </tr>
-                </thead>
-                <tbody className='text-center'>
-                    {students.map((student, index) => (
+  };
 
-                    <tr key={student.id}>
-                        <th scope='row' key={index}>
-                            {index + 1}
-                        </th>
-                        <td>{student.firstName}</td>
-                        <td>{student.lastName}</td>
-                        <td>{student.email}</td>
-                        <td>{student.department}</td>
-                        <td className='mx-2'>
-                            <button className="btn btn-info">
-                            View
-                            </button>
-                        </td>
-                        <td className='mx-2'>
-                            <button className="btn btn-warning">
-                            Update
-                            </button>
-                        </td>
-                        <td className='mx-2'>
-                            <button className='btn btn-danger'>Delete</button>
-                        </td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
-        </section>
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:8080/api/student/${id}`);
+    loadStudents();
+  };
+  return (
+    <section>
+      <table className="table table-bordered table-hover shadow">
+        <thead>
+          <tr className="text-center">
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th colSpan="3">Actions</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {students.map((student, index) => (
+            <tr key={student.id}>
+              <th scope="row" key={index}>
+                {index + 1}
+              </th>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.email}</td>
+              <td>{student.department}</td>
+              <td className="mx-2">
+                <Link
+                  to={`/student-profile/${student.id}`}
+                  className="btn btn-info">
+                  <FaEye />
+                </Link>
+              </td>
+              <td className="mx-2">
+                <Link
+                  to={`/edit-student/${student.id}`}
+                  className="btn btn-warning">
+                  <FaEdit />
+                </Link>
+              </td>
+              <td className="mx-2">
+                <button
+                  className="btn btn-danger"
+                  onClick={(e) => handleDelete(student.id)}>
+                  <FaTrashAlt />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
