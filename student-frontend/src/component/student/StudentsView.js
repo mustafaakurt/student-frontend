347722ea997078f27d2f33c23539f9ad0b2ade95
mustafaakurt <mustafaakurt@yandex.com>
2024-05-16 03:34:44 +0300

    search added

--- student-frontend/src/component/student/StudentsView.js
+++ student-frontend/src/component/student/StudentsView.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { FaTrashAlt, FaEdit, FaEye } from "react-icons/fa";
+import Search from "../common/Search";
 
 const StudentsView = () => {
   const [students, setStudents] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(() => {
     loadStudents();
@@ -27,6 +29,7 @@ const StudentsView = () => {
   };
   return (
     <section>
+      <Search search={search} setSearch={setSearch} />
       <table className="table table-bordered table-hover shadow">
         <thead>
           <tr className="text-center">
@@ -39,38 +42,44 @@ const StudentsView = () => {
           </tr>
         </thead>
         <tbody className="text-center">
-          {students.map((student, index) => (
-            <tr key={student.id}>
-              <th scope="row" key={index}>
-                {index + 1}
-              </th>
-              <td>{student.firstName}</td>
-              <td>{student.lastName}</td>
-              <td>{student.email}</td>
-              <td>{student.department}</td>
-              <td className="mx-2">
-                <Link
-                  to={`/student-profile/${student.id}`}
-                  className="btn btn-info">
-                  <FaEye />
-                </Link>
-              </td>
-              <td className="mx-2">
-                <Link
-                  to={`/edit-student/${student.id}`}
-                  className="btn btn-warning">
-                  <FaEdit />
-                </Link>
-              </td>
-              <td className="mx-2">
-                <button
-                  className="btn btn-danger"
-                  onClick={(e) => handleDelete(student.id)}>
-                  <FaTrashAlt />
-                </button>
-              </td>
-            </tr>
-          ))}
+          {students
+            .filter(
+              (st) =>
+                st.firstName.toLowerCase().includes(search) ||
+                st.lastName.toLowerCase().includes(search)
+            )
+            .map((student, index) => (
+              <tr key={student.id}>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.email}</td>
+                <td>{student.department}</td>
+                <td className="mx-2">
+                  <Link
+                    to={`/student-profile/${student.id}`}
+                    className="btn btn-info">
+                    <FaEye />
+                  </Link>
+                </td>
+                <td className="mx-2">
+                  <Link
+                    to={`/edit-student/${student.id}`}
+                    className="btn btn-warning">
+                    <FaEdit />
+                  </Link>
+                </td>
+                <td className="mx-2">
+                  <button
+                    className="btn btn-danger"
+                    onClick={(e) => handleDelete(student.id)}>
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </section>
